146dddf61e7868390021b3578502caf2175f560e
Martin <bumko96@gmail.com>
2025-09-03 10:34:33 +0200

    added home component

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello universe!</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  template: `<main>
+    <header class="brand-name">
+      <img
+        class="brand-logo"
+        src="/assets/logo.svg"
+        alt="logo"
+        aria-hidden="true"
+      />
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </header>
+  </main>`,
+  styleUrls: ["./app.component.css"],
+  imports: [HomeComponent]
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes";
 }
